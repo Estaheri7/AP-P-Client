6345ee20cb1a4161c4750b382d12f0cbb2be1ec2
erfansalman <estaheri101@gmail.com>
2024-06-23 21:26:11 +0330

    add: added login Fxml

--- src/main/java/org/example/appclient/LinkedInApp.java
+++ src/main/java/org/example/appclient/LinkedInApp.java
@@ -1,6 +1,9 @@
 package org.example.appclient;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LinkedInApp extends Application {
@@ -10,6 +13,11 @@ public class LinkedInApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("LinkedIn App");
+        stage.show();
     }
 }
