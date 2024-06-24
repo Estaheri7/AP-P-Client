857d3d7c551a8a9036075dda7812864593d489f4
erfansalman <estaheri101@gmail.com>
2024-06-24 17:41:45 +0330

    add requires to modules-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.example.appclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
     opens org.example.appclient to javafx.fxml;
     exports org.example.appclient;
+    exports org.example.appclient.Controllers;
+    opens org.example.appclient.Controllers to javafx.fxml;
 }
