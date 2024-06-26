0c59de0c2ec2c02e680edf2d79c9aa5adcc81338
erfansalman <estaheri101@gmail.com>
2024-06-26 18:36:44 +0330

    add: added decoder for jwt payload in JwtManager

--- src/main/java/org/example/appclient/util/JwtManager.java
+++ src/main/java/org/example/appclient/util/JwtManager.java
@@ -1,6 +1,13 @@
 package org.example.appclient.util;
 
+import com.google.gson.Gson;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+
 public class JwtManager {
+    private static final Gson gson = new Gson();
     private static String jwtToken;
 
     public static void setJwtToken(String token) {
@@ -13,5 +20,15 @@ public class JwtManager {
 
     public static boolean isJwtTokenAvailable() {
         return jwtToken != null;
+    }
+
+    public static Object decodeJwtPayload(String jwt) {
+        String[] parts = jwt.split("\\.");
+        if (parts.length < 2) {
+            throw new IllegalArgumentException("Invalid JWT token.");
+        }
+
+        String payloadJson = new String(Base64.getUrlDecoder().decode(parts[1]), StandardCharsets.UTF_8);
+        return gson.fromJson(payloadJson, Map.class).get("sub");
     }
 }
