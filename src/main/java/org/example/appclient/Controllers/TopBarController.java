44ee8e76b247d88e1c5800ecdc3984435d30a18e
mahdioudi <mahdioudi5@gmail.com>
2024-06-28 02:07:15 +0330

    add: added TopBarController class

---
+++ src/main/java/org/example/appclient/Controllers/TopBarController.java
@@ -0,0 +1,35 @@
+package org.example.appclient.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class TopBarController {
+
+    @FXML
+    private ImageView homeImageView;
+
+    @FXML
+    private ImageView usersImageView;
+
+    @FXML
+    private ImageView meImageView;
+
+    @FXML
+    private ImageView messagingImageView;
+
+    @FXML
+    private ImageView myNetworkImageView;
+
+    @FXML
+    private TextField searchTextField;
+
+    private void onHomeClicked() {}
+    private void onUsersClicked() {}
+    private void onMeClicked() {}
+    private void onMessagingClicked() {}
+    private void onMyNetworkClicked() {}
+
+
+}
