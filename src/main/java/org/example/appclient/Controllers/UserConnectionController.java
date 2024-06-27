d4d017a65acdf9c66e12cac9354ec297e6d83d3a
mahdioudi <mahdioudi5@gmail.com>
2024-06-28 02:08:40 +0330

    refactor

--- src/main/java/org/example/appclient/Controllers/UserConnectionController.java
+++ src/main/java/org/example/appclient/Controllers/UserConnectionController.java
@@ -1,7 +1,6 @@
 package org.example.appclient.Controllers;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -21,21 +20,19 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.example.appclient.util.JwtManager;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 
 public class UserConnectionController {
 
     private final Gson gson = new Gson();
+
+    private final FetcherEmail fetcher = new FetcherEmail();
 
     @FXML
     private ScrollPane connectionsScrollPane;
@@ -44,220 +41,25 @@ public class UserConnectionController {
     private TextField searchByName;
 
     @FXML
-    private VBox connectionsVBox;
+    private VBox usersVBox;
 
     @FXML
     private Label connectionLabel;
 
-
-    public ArrayList<String> fetchSenderEmails() {
-        ArrayList<String> senderEmails = new ArrayList<>();
-
-        if (JwtManager.isJwtTokenAvailable()) {
-            HttpURLConnection connection = null;
-            try {
-                URL url = new URL("http://localhost:8080/connections/" + ProfileController.getProfileEmail());
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Authorization", "Bearer " + JwtManager.getJwtToken());
-
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                        StringBuilder response = new StringBuilder();
-                        String inputLine;
-                        while ((inputLine = br.readLine()) != null) {
-                            response.append(inputLine);
-                        }
-                        ArrayList<HashMap<String, String>> connections = gson.fromJson(response.toString(), new TypeToken<List<HashMap<String, String>>>(){}.getType());
-                        for (HashMap<String, String> connect : connections) {
-                            String sender = connect.get("sender");
-                            if (sender != null) {
-                                senderEmails.add(sender);
-                            }
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-        }
-        return senderEmails;
-    }
-
-    public HashMap<String, String> fetchProfileDetailsByEmail(String senderEmail) {
-        HttpURLConnection connection = null;
-        HashMap<String, String> profileDetails = new HashMap<>();
-
-        if (JwtManager.isJwtTokenAvailable()) {
-            try {
-                URL url = new URL("http://localhost:8080/user/" + senderEmail);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                        StringBuilder response = new StringBuilder();
-                        String line;
-                        while ((line = br.readLine()) != null) {
-                            response.append(line);
-                        }
-
-                        Type type = new TypeToken<HashMap<String, String>>() {}.getType();
-                        profileDetails = gson.fromJson(response.toString(), type);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-        }
-
-        return profileDetails;
-    }
-
     public void displayConnections() {
-        ArrayList<String> senderEmails = fetchSenderEmails();
+        ArrayList<String> senderEmails = FetcherEmail.fetchEmails("connections", "sender");
         connectionLabel.setText(senderEmails.size() + " Connection");
-        connectionsVBox.getChildren().clear();
+        usersVBox.getChildren().clear();
         for (String senderEmail : senderEmails) {
-            HashMap<String, String> userDetail = fetchProfileDetailsByEmail(senderEmail);
-            addConnectionEntry(userDetail);
-        }
-    }
-
-    private void addConnectionEntry(HashMap<String, String> userDetail) {
-        String email = userDetail.get("email");
-        String name = userDetail.get("firstName");
-        String lastName = userDetail.get("lastName");
-        String headline = userDetail.get("headline");
-        String city = userDetail.get("city");
-        String country = userDetail.get("country");
-        String avatarURL = userDetail.get("avatar_url");
-
-        HBox connectionEntry = new HBox();
-        connectionEntry.setSpacing(10);
-        connectionEntry.setPrefWidth(650);
-
-        if (avatarURL != null && !avatarURL.isEmpty()) {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080/user/avatar/" + email).openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Authorization", "Bearer " + JwtManager.getJwtToken());
-
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    InputStream inputStream = connection.getInputStream();
-                    Image image = new Image(inputStream);
-                    ImageView imageView = new ImageView(image);
-                    imageView.setFitWidth(50);
-                    imageView.setFitHeight(50);
-                    imageView.setOnMouseClicked(event -> goToProfile(email));
-                    connectionEntry.getChildren().add(imageView);
-                    inputStream.close();
-                } else {
-                    Image image = new Image(getClass().getResource("/org/example/appclient/images/linkedInIcon.png").toExternalForm());
-                    ImageView imageView = new ImageView(image);
-                    imageView.setFitWidth(50);
-                    imageView.setFitHeight(50);
-                    imageView.setOnMouseClicked(event -> goToProfile(email));
-                    connectionEntry.getChildren().add(imageView);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        VBox userDetailsVBox = new VBox();
-        userDetailsVBox.setPrefWidth(480);
-        Label nameLabel = new Label(name + " " + lastName);
-        nameLabel.setFont(new Font(18));
-        nameLabel.setFont(Font.font("System", FontWeight.BOLD, 18));
-        Label headlineLabel = new Label(headline);
-        Label locationLabel = new Label(city + ", " + country);
-        userDetailsVBox.getChildren().addAll(nameLabel, headlineLabel, locationLabel);
-
-//        int nameSize = (name + lastName).length();
-        Button button = new Button("Connect");
-        button.setTranslateY(25);
-//        messageButton.setTranslateX(nameSize * 7 + 300);
-        onButton(button, email);
-        connectionEntry.setPadding(new Insets(10, 10, 10, 10));
-        connectionEntry.getChildren().addAll(userDetailsVBox, button);
-        connectionsVBox.getChildren().add(connectionEntry);
-    }
-
-    private void onButton(Button button, String email) {
-        if (ConnectController.isConnectionPending(email)) {
-            button.setText("Pending");
-            button.setDisable(true);
-        } else if (ConnectController.isConnected(email)) {
-            button.setText("Message");
-        }
-        button.setOnAction(event -> {
-            if (button.getText().equals("Connect")) {
-                loadSendConnection(button);
-            } else if (button.getText().equals("Message")) {
-                // TODO go to message room
-                System.out.println("Go to message");
-            }
-        });
-    }
-
-    private void loadSendConnection(Button btn) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/appclient/sendConnection.fxml"));
-            Parent root = loader.load();
-
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("send connection");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(btn.getScene().getWindow());
-            dialogStage.setResizable(false);
-            Scene scene = new Scene(root);
-            dialogStage.setScene(scene);
-            dialogStage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
+            HashMap<String, String> userDetail = FetcherEmail.fetchProfileDetailsByEmail(senderEmail);
+            fetcher.addEntry(userDetail, usersVBox, connectionLabel);
         }
     }
 
     public void initialize() {
-        connectionsVBox = new VBox();
-        connectionsScrollPane.setContent(connectionsVBox);
+        usersVBox = new VBox();
+        connectionsScrollPane.setContent(usersVBox);
         displayConnections();
-    }
-
-    private void goToProfile(String email) {
-        try {
-            ProfileController.setProfileEmail(email);
-
-            Parent profilePage = FXMLLoader.load(getClass().getResource("/org/example/appclient/profile.fxml"));
-            Stage currentStage = (Stage) connectionLabel.getScene().getWindow();
-            Scene scene = new Scene(profilePage);
-
-            Stage profileStage = new Stage();
-            profileStage.setScene(scene);
-            profileStage.setTitle("Profile");
-            profileStage.initOwner(currentStage);
-            profileStage.initModality(Modality.WINDOW_MODAL);
-
-            profileStage.setFullScreen(true);
-            profileStage.setFullScreenExitHint("");
-
-            currentStage.hide();
-            profileStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
 
